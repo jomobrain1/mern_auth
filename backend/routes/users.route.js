ad16d0f481f5b65dd665b6f39fd51d71a2eaa08e
jomobrain1 <jomobrain1@gmail.com>
2026-02-22 16:27:33 +0300

    Done changepassword and verify password

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -4,6 +4,9 @@ const {
   verifyUser,
   loginUser,
   logoutUser,
+  forgotPassword,
+  verifyOTP,
+  changePassword,
 } = require("../controllers/users.controller");
 const { isAuthenticated } = require("../middlewares/auth.middleware");
 
@@ -13,5 +16,8 @@ router.post("/auth/register", registerUser);
 router.post("/auth/verify", verifyUser);
 router.post("/auth/login", loginUser);
 router.post("/auth/logout", isAuthenticated, logoutUser);
+router.post("/auth/forgot-password", forgotPassword);
+router.post("/auth/verify-otp/:email", verifyOTP);
+router.post("/auth/change-password/:email", changePassword);
 
 module.exports = router;
